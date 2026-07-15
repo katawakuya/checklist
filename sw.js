bbd162c67786e64af7f47ce3250c42694ba9b090
katawakuya <sato@k-katawakuya.com>
2026-07-16 02:46:21 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker - 現場管理システム
 // キャッシュバージョン（更新時はここを変える）
-const CACHE_VERSION = 'v3.4';
+const CACHE_VERSION = 'v3.5';
 const CACHE_NAME = 'kanri-' + CACHE_VERSION;
 
 // キャッシュするファイル一覧
